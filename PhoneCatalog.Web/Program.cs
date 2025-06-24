4ceb6aa4da8ba7914c6362c1c180501980181ff8
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-06-24 19:16:29 +0300

    Για να καλεί το connection μόνο του

--- PhoneCatalog.Web/Program.cs
+++ PhoneCatalog.Web/Program.cs
@@ -9,7 +9,8 @@ class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddDbContext<Repository>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDbContext<Repositories>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
         var app = builder.Build();
 
         // Configure the HTTP request pipeline. Or else Middleware
